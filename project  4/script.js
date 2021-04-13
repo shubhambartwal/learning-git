0515e2ab0b7400b576ad3b5b163ab1718e5ac186
Shubham Bartwal <shubhambartwal58@gmail.com>
2021-04-13 23:18:39 +0530

    proect 4

---
+++ project  4/script.js
@@ -0,0 +1,14 @@
+const sendBtn=document.querySelector('#sendBtn');
+const messageIn=document.querySelector('#messageIn');
+const messageOut=document.querySelector('#messageOut');
+sendBtn.addEventListener('click',sendMsg);
+function sendMsg(){
+    let content=messageIn.value; //catch the value of input
+    //console.log(content);
+    if(content==='')
+    alert("Please enter a valid value");
+    else{
+        messageOut.innerHTML=content;
+    messageIn.value="";//making value empty
+    }
+}
